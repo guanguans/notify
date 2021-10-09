9de8c88a673f9dd6059301634e6897d5585d2cef
yaozm <ityaozm@gmail.com>
2021-10-09 16:36:35 +0800

    Update CI config files

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,4 +8,50 @@
  * This source file is subject to the MIT license that is bundled.
  */
 
-return require __DIR__.'/.php_cs.dist';
+$header = <<<EOF
+This file is part of the guanguans/notify.
+
+(c) guanguans <ityaozm@gmail.com>
+
+This source file is subject to the MIT license that is bundled.
+EOF;
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__,
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->exclude([
+        __DIR__.'/vendor',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->notName('_ide_helper.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        // '@PSR12' => true,
+        '@Symfony' => true,
+        'header_comment' => [
+            'header' => $header,
+            'comment_type' => 'PHPDoc'
+        ],
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'single_quote' => true,
+        'class_attributes_separation' => true,
+        'standardize_not_equals' => true,
+        // 'trailing_comma_in_multiline' => true,
+        // 'php_unit_construct' => true,
+        // 'php_unit_strict' => true,
+        // 'declare_strict_types' => true,
+    ])
+    // ->setRiskyAllowed(true)
+    ->setFinder($finder);
