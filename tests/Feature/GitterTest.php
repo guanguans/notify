48c434dbdc111e595459093aa4d66e8ef249499d
yaozm <ityaozm@gmail.com>
2021-12-07 18:10:06 +0800

    Add tests

---
+++ tests/Feature/GitterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Tests\Feature;
+
+use Guanguans\Notify\Factory;
+use Guanguans\Notify\Tests\TestCase;
+
+class GitterTest extends TestCase
+{
+    public function testGitter()
+    {
+        $this->expectException(\GuzzleHttp\Exception\ClientException::class);
+
+        Factory::gitter()
+            ->setToken('b9e7931ecacb08b7ab4df5e98bc149d33d7faf1')
+            ->setRoomId('61af21b96da03739848bfef')
+            ->setMessage(new \Guanguans\Notify\Messages\GitterMessage('This is testing.'))
+            ->send();
+    }
+}
