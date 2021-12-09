7a3e5a20b9fdc4015f086a14df11d33cf5119939
yaozm <ityaozm@gmail.com>
2021-12-09 18:03:32 +0800

    Add support for `Mattermost`

---
+++ src/Clients/MattermostClient.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Clients;
+
+/**
+ * @see https://api.mattermost.com
+ *
+ * ```
+ * // user login.
+ * curl --location --request POST 'https://guanguans.cloud.mattermost.com/api/v4/users/login' \
+ * --header 'Content-Type: application/json' \
+ * --data-raw '{"login_id":"{{user_name}}","password":"{{password}}"}'
+ *
+ * // channel list.
+ * curl --location --request GET 'https://guanguans.cloud.mattermost.com/api/v4/channels' \
+ * --header 'Authorization: Bearer {{token}}' \
+ * --header 'Content-Type: application/json' \
+ *
+ * // send a post.
+ * curl --location --request POST 'https://guanguans.cloud.mattermost.com/api/v4/posts' \
+ * --header 'Authorization: Bearer {{token}}' \
+ * --header 'Content-Type: application/json' \
+ * --data-raw '{"message":"This is a testing.","channel_id":"{{channel_id}}"}'
+ * ```
+ */
+class MattermostClient extends Client
+{
+    public const REQUEST_URL_TEMPLATE = '%s/api/v4/posts';
+
+    public $requestMethod = 'postJson';
+
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'token',
+        'message',
+        'host',
+        'channel_id',
+    ];
+
+    public function __construct(array $options = [])
+    {
+        parent::__construct($options);
+
+        $this->sending(function (self $client) {
+            $client->setHttpOptions([
+                'headers' => [
+                    'Authorization' => 'Bearer '.$client->getToken(),
+                ],
+            ]);
+        });
+    }
+
+    public function getRequestUrl(): string
+    {
+        return sprintf(self::REQUEST_URL_TEMPLATE, rtrim($this->getHost(), '/'));
+    }
+
+    public function getHost(): string
+    {
+        return $this->getOption('host');
+    }
+
+    /**
+     * @return $this
+     */
+    public function setHost(string $host): self
+    {
+        $this->setOption('host', $host);
+
+        return $this;
+    }
+
+    public function getChannelId(): string
+    {
+        return $this->getOption('channel_id');
+    }
+
+    /**
+     * @return $this
+     */
+    public function setChannelId(string $channelId): self
+    {
+        $this->setOption('channel_id', $channelId);
+
+        return $this;
+    }
+
+    public function getRequestParams(): array
+    {
+        return array_merge(parent::getRequestParams(), [
+            'channel_id' => $this->getChannelId(),
+        ]);
+    }
+}
---
+++ src/Messages/MattermostMessage.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Messages;
+
+class MattermostMessage extends Message
+{
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'channel_id',
+        'message',
+        'file_ids',
+        'create_at',
+        'edit_at',
+        'is_pinned',
+        'root_id',
+        'original_id',
+        'type',
+        'props',
+        'pending_post_id',
+        'participants',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $required = [
+        // 'channel_id',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $allowedTypes = [
+        'file_ids' => 'array',
+        'props' => 'array',
+        'is_pinned' => 'bool',
+        'create_at' => 'int',
+        'edit_at' => 'int',
+    ];
+}
