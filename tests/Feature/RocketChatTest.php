ff30031849713f702682c76ab96942bdb66ed05b
yaozm <ityaozm@gmail.com>
2021-12-09 14:58:43 +0800

    Add tests

---
+++ tests/Feature/RocketChatTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Tests\Feature;
+
+use Guanguans\Notify\Factory;
+use Guanguans\Notify\Tests\TestCase;
+
+class RocketChatTest extends TestCase
+{
+    public function testRocketChat()
+    {
+        $this->expectException(\GuzzleHttp\Exception\ClientException::class);
+
+        Factory::rocketChat()
+            ->setToken('EemSHx9ioqdmrWouS/yYpmhqDSyd7CqmSAnyBfKezLyzotswbRSpkD9MCNxqtPL')
+            ->setHost('https://guanguans.rocket.chat')
+            ->setMessage(
+                new \Guanguans\Notify\Messages\RocketChatMessage([
+                    'alias' => '报警机器人',
+                    'emoji' => ':warning:',
+                    'text' => 'This is a testing. ',
+                    'attachments' => [
+                        [
+                            'title' => 'This is a title.',
+                            'title_link' => 'https://rocket.chat',
+                            'text' => 'This is a text.',
+                            'image_url' => 'http://www.xxx.png',
+                            'color' => '#764FA5',
+                        ],
+                    ],
+                ])
+            )
+            ->send();
+    }
+}
