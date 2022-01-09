5f9be407c9244ccbf4d061ea9a843eded3ed9418
yaozm <ityaozm@gmail.com>
2022-01-09 18:04:09 +0800

    Update tests

---
+++ tests/Feature/IGotTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Tests\Feature;
+
+use Guanguans\Notify\Factory;
+use Guanguans\Notify\Tests\TestCase;
+
+class IGotTest extends TestCase
+{
+    public function testIGot()
+    {
+        $this->markTestSkipped(__CLASS__.' is skipped.');
+
+        $ret = Factory::iGot()
+            ->setToken('5dcd2f91d38cc47447414')
+            ->setMessage(
+                new \Guanguans\Notify\Messages\IGotMessage([
+                    'content' => 'This is content.',
+                    // 'title' => 'This is title.',
+                    // 'url' => 'https://www.github.com/guanguans/notify',
+                    // 'automaticallyCopy' => 1,
+                    // 'urgent' => 1,
+                    // 'copy' => 'This is copy.',
+                    // 'detail' => [
+                    //     'title' => 'This is detail title.',
+                    //     'content' => 'This is detail content.',
+                    // ],
+                ])
+            )
+            ->send();
+
+        $this->assertEquals('201', $ret['ret']);
+    }
+}
