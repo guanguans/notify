6b224b16d48692382a3984c8470bcf1493dbcd29
yaozm <ityaozm@gmail.com>
2024-01-23 16:41:16 +0800

    wip Zulip

---
+++ src/Zulip/Credential.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Zulip;
+
+use Guanguans\Notify\Ntfy\BasicAuthCredential;
+
+class Credential extends BasicAuthCredential
+{
+    public function __construct(string $email, string $token)
+    {
+        parent::__construct($email, $token);
+    }
+}
---
+++ src/Zulip/Messages/PrivateMessage.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Zulip\Messages;
+
+class PrivateMessage extends Message
+{
+    public function __construct(string $to, string $content)
+    {
+        parent::__construct([
+            'to' => $to,
+            'content' => $content,
+        ]);
+    }
+
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'type',
+        'to',
+        'content',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $required = [
+        'type',
+        'to',
+        'content',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $defaults = [
+        'type' => 'private',
+    ];
+
+    /**
+     * @var \string[][]
+     */
+    protected $allowedValues = [
+        'type' => ['private'],
+    ];
+}
