51d548eeb6ffe1b8fe1a9c5de0d35bc9be6aba63
yaozm <ityaozm@gmail.com>
2024-01-23 15:54:09 +0800

    wip Showdoc

---
+++ src/Showdoc/Messages/Message.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Showdoc\Messages;
+
+use Guanguans\Notify\Foundation\Concerns\AsJson;
+use Guanguans\Notify\Foundation\Concerns\AsPost;
+use Guanguans\Notify\Showdoc\Credential;
+
+class Message extends \Guanguans\Notify\Foundation\Message
+{
+    use AsPost;
+    use AsJson;
+
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'title',
+        'content',
+    ];
+
+    public function __construct(string $title, string $content)
+    {
+        parent::__construct([
+            'title' => $title,
+            'content' => $content,
+        ]);
+    }
+
+    public function httpUri()
+    {
+        return sprintf('https://push.showdoc.com.cn/server/api/push/%s', Credential::TEMPLATE_TOKEN);
+    }
+}
