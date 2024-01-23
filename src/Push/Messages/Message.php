bc089d48fd00b2aa17f768ca73db38891ba055e7
yaozm <ityaozm@gmail.com>
2024-01-23 15:16:12 +0800

    wip Push

---
+++ src/Push/Messages/Message.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Push\Messages;
+
+use Guanguans\Notify\Foundation\Concerns\AsJson;
+use Guanguans\Notify\Foundation\Concerns\AsPost;
+use Guanguans\Notify\Push\Credential;
+
+class Message extends \Guanguans\Notify\Foundation\Message
+{
+    use AsPost;
+    use AsJson;
+
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'title',
+        'body',
+        'link',
+        'image',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $required = [
+        'title',
+        'body',
+    ];
+
+    public function httpUri()
+    {
+        return sprintf('https://push.techulus.com/api/v1/notify/%s', Credential::TEMPLATE_TOKEN);
+    }
+}
