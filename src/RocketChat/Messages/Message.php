adb1ba69d4230fa5d2721440a8353b434bb628c6
yaozm <ityaozm@gmail.com>
2024-01-23 15:43:22 +0800

    wip RocketChat

---
+++ src/RocketChat/Messages/Message.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\RocketChat\Messages;
+
+use Guanguans\Notify\Foundation\Concerns\AsJson;
+use Guanguans\Notify\Foundation\Concerns\AsPost;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Message extends \Guanguans\Notify\Foundation\Message
+{
+    use AsPost;
+    use AsJson;
+
+    /**
+     * @var string[]
+     */
+    protected $defined = [
+        'alias',
+        'emoji',
+        'text',
+        'attachments',
+    ];
+
+    /**
+     * @var string|array[]
+     */
+    protected $allowedTypes = [
+        'attachments' => ['array'],
+    ];
+
+    /**
+     * @var array
+     */
+    protected $options = [
+        'attachments' => [],
+    ];
+
+    public function __construct(array $options = [])
+    {
+        parent::__construct($options);
+
+        $this->addAttachments($options['attachments'] ?? []);
+    }
+
+    public function setAttachments(array $attachments): self
+    {
+        return $this->addAttachments($attachments);
+    }
+
+    public function addAttachments(array $attachments): self
+    {
+        foreach ($attachments as $attachment) {
+            $this->addAttachment($attachment);
+        }
+
+        return $this;
+    }
+
+    public function setAttachment(array $attachment): self
+    {
+        return $this->addAttachment($attachment);
+    }
+
+    public function addAttachment(array $attachment): self
+    {
+        $this->options['attachments'][] = configure_options($attachment, static function (OptionsResolver $optionsResolver): void {
+            $optionsResolver->setDefined([
+                'title',
+                'title_link',
+                'text',
+                'image_url',
+                'color',
+            ]);
+        });
+
+        return $this;
+    }
+
+    public function httpUri()
+    {
+        return '{base_uri}/hooks/{token}';
+    }
+}
