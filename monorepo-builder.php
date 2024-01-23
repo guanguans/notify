7273577aab77a85fdfaec9ebc1e91b440b36a205
yaozm <ityaozm@gmail.com>
2024-01-23 18:06:38 +0800

    chore: Update composer.json and monorepo-builder.php
    - Update `composer.json` to use `@php` instead of absolute path for `ai-commit` and `release` commands.
    - Update `monorepo-builder.php` to use relative path for `monorepo-builder-worker` autoload file.
    - Update `phpunit.xml.dist` to use schema version 9.3 and configure code coverage.
    - Update `tests/Pest.php` with annotations.

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -26,7 +26,7 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorke
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
-    require __DIR__.'/vendor-bin/monorepo-builder-worker/vendor/autoload.php';
+    require __DIR__.'/vendor/autoload.php';
     $mbConfig->defaultBranch('main');
 
     /**
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,6 @@
 <?php
 
+/** @noinspection AnonymousFunctionStaticInspection */
 /** @noinspection DuplicateCustomExpectationInspection */
 
 declare(strict_types=1);
