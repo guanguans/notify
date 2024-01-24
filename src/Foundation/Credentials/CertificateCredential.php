6b8e576642916f02f4635f694bca061d8149b54f
yaozm <ityaozm@gmail.com>
2024-01-24 16:45:11 +0800

    feat(credentials): add CertificateCredential class
    - Added CertificateCredential class to handle certificate credentials
    - Implements the applyToOptions method to apply the certificate to the request options

---
+++ src/Foundation/Credentials/CertificateCredential.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Notify\Foundation\Credentials;
+
+use GuzzleHttp\RequestOptions;
+
+class CertificateCredential extends NullCredential
+{
+    private string $path;
+    private ?string $password;
+
+    public function __construct(string $path, string $password = null)
+    {
+        $this->path = $path;
+        $this->password = $password;
+    }
+
+    public function applyToOptions(array $options): array
+    {
+        return [
+            RequestOptions::CERT => is_string($this->password) ? [$this->path, $this->password] : $this->path,
+        ] + $options;
+    }
+}
