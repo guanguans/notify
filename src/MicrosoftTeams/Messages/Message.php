41d04197cb2a57a9f503b6d77dede616a300e491
yaozm <ityaozm@gmail.com>
2024-01-30 18:24:24 +0800

    wip MicrosoftTeams

--- src/MicrosoftTeams/Messages/Message.php
+++ src/MicrosoftTeams/Messages/Message.php
@@ -64,122 +64,100 @@ class Message extends \Guanguans\Notify\Foundation\Message
         'potentialAction' => [],
     ];
 
-    public function setSections(array $sections): self
-    {
-        return $this->addSections($sections);
-    }
-
-    public function addSections(array $sections): self
-    {
-        foreach ($sections as $section) {
-            $this->addSection($section);
-        }
-
-        return $this;
-    }
-
-    public function setSection(array $section): self
-    {
-        return $this->addSection($section);
-    }
-
     public function addSection(array $section): self
     {
-        $this->options['sections'][] = configure_options($section, static function (OptionsResolver $optionsResolver): void {
-            $optionsResolver
-                ->setDefined([
-                    'title',
-                    'startGroup',
-                    'activityImage',
-                    'activityTitle',
-                    'activitySubtitle',
-                    'activityText',
-                    'heroImage',
-                    'text',
-                    'facts',
-                    'images',
-                    'potentialAction',
-                ])
-                ->setAllowedTypes('startGroup', 'bool')
-                ->setAllowedTypes('facts', 'array')
-                ->setAllowedTypes('images', 'array')
-                ->setAllowedTypes('potentialAction', 'array');
-        });
+        $this->options['sections'][] = configure_options(
+            $section,
+            static function (OptionsResolver $optionsResolver): void {
+                $optionsResolver
+                    ->setDefined([
+                        'title',
+                        'startGroup',
+                        'activityImage',
+                        'activityTitle',
+                        'activitySubtitle',
+                        'activityText',
+                        'heroImage',
+                        'text',
+                        'facts',
+                        'images',
+                        'potentialAction',
+                    ])
+                    ->setAllowedTypes('startGroup', 'bool')
+                    ->setAllowedTypes('facts', 'array')
+                    ->setAllowedTypes('images', 'array')
+                    ->setAllowedTypes('potentialAction', 'array');
+            }
+        );
 
         return $this;
-    }
-
-    public function setPotentialActions(array $sections): self
-    {
-        return $this->addPotentialActions($sections);
-    }
-
-    public function addPotentialActions(array $sections): self
-    {
-        foreach ($sections as $section) {
-            $this->addPotentialAction($section);
-        }
-
-        return $this;
-    }
-
-    public function setPotentialAction(array $section): self
-    {
-        return $this->addPotentialAction($section);
     }
 
     public function addPotentialAction(array $section): self
     {
-        $this->options['potentialAction'][] = configure_options($section, static function (OptionsResolver $optionsResolver): void {
-            $optionsResolver
-                ->setDefined([
-                    '@type',
-                    'name',
-                    'targets',
+        $this->options['potentialAction'][] = configure_options(
+            $section,
+            static function (OptionsResolver $optionsResolver): void {
+                $optionsResolver
+                    ->setDefined([
+                        '@type',
+                        'name',
+                        'targets',
 
-                    'target',
-                    'headers',
-                    'body',
-                    'bodyContentType',
+                        'target',
+                        'headers',
+                        'body',
+                        'bodyContentType',
 
-                    'inputs',
-                    'actions',
+                        'inputs',
+                        'actions',
 
-                    'addInId',
-                    'desktopCommandId',
-                    'initializationContext',
-                ])
-                ->setAllowedTypes('targets', 'array')
-                ->setAllowedTypes('headers', 'array')
-                ->setAllowedTypes('inputs', 'array')
-                ->setAllowedTypes('actions', 'array')
-                ->setAllowedTypes('initializationContext', 'array')
-                ->addAllowedValues('@type', [
-                    'OpenUri',
-                    'ActionCard',
-                    'HttpPOST',
-                    'InvokeAddInCommand',
-                ])
-                ->addAllowedValues('bodyContentType', [
-                    'application/x-www-form-urlencoded',
-                    'application/json',
-                ]);
-        });
+                        'addInId',
+                        'desktopCommandId',
+                        'initializationContext',
+                    ])
+                    ->setAllowedTypes('targets', 'array')
+                    ->setAllowedTypes('headers', 'array')
+                    ->setAllowedTypes('inputs', 'array')
+                    ->setAllowedTypes('actions', 'array')
+                    ->setAllowedTypes('initializationContext', 'array')
+                    ->addAllowedValues('@type', [
+                        'OpenUri',
+                        'ActionCard',
+                        'HttpPOST',
+                        'InvokeAddInCommand',
+                    ])
+                    ->addAllowedValues('bodyContentType', [
+                        'application/x-www-form-urlencoded',
+                        'application/json',
+                    ]);
+            }
+        );
 
         return $this;
     }
 
     public function toHttpUri()
     {
-        return 'webhook';
+        // return 'webhook';
+        return '';
     }
 
     protected function configureOptionsResolver(OptionsResolver $optionsResolver): OptionsResolver
     {
-        return tap(parent::configureOptionsResolver($optionsResolver), static function (OptionsResolver $resolver): void {
-            $resolver->setNormalizer('sections', static fn (OptionsResolver $optionsResolver, array $value): array => isset($value[0]) ? $value : [$value]);
+        return tap(
+            parent::configureOptionsResolver($optionsResolver),
+            static function (OptionsResolver $resolver): void {
+                $resolver->setNormalizer('sections', static fn (
+                    OptionsResolver $optionsResolver,
+                    array $value
+                ): array => isset($value[0]) ? $value : [$value]);
 
-            $resolver->setNormalizer('potentialAction', static fn (OptionsResolver $optionsResolver, array $value): array => isset($value[0]) ? $value : [$value]);
-        });
+                $resolver->setNormalizer('potentialAction', static fn (
+                    OptionsResolver $optionsResolver,
+                    array $value
+                ): array => isset($value[0]) ? $value : [$value]);
+            }
+        );
     }
 }
