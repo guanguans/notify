808b6b6acb27f60f5ffa94668433d3c1020da2b0
yaozm <ityaozm@gmail.com>
2024-02-19 17:39:45 +0800

    test(ClientTest): Add test case for sending message
    - Added a new test case in ClientTest.php to test the functionality of sending a message using the Client class and Message class
    - Included assertions to check the response instances after sending the message

---
+++ tests/Ntfy/ClientTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/** @noinspection StaticClosureCanBeUsedInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\NotifyTests\Ntfy;
+
+use Guanguans\Notify\Ntfy\Client;
+use Guanguans\Notify\Ntfy\Messages\Message;
+use Psr\Http\Message\ResponseInterface;
+
+it('can send message', function (): void {
+    $client = new Client;
+    $message = Message::make([
+        'topic' => 'guanguans',
+        'message' => 'This is message.',
+        'title' => 'This is title.',
+        'priority' => 1,
+        'tags' => ['tag1', 'tag2'],
+        'click' => 'https://example.com',
+        'attach' => 'https://www.guanguans.cn',
+        'icon' => 'https://www.guanguans.cn',
+        'filename' => 'file.jpg',
+        'cache' => 'no',
+        'firebase' => 'no',
+        // 'actions' => [],
+        // 'delay' => '30min, 9am',
+        // 'email' => 'xxx@qq.com',
+    ]);
+
+    expect($client)
+        ->httpErrors(false)
+        ->mock([
+            create_response('{"id":"ChjDFVOPqaBK","time":1708335367,"expires":1708378567,"event":"message","topic":"guanguans","title":"This is title.","message":"This is message.","priority":1,"tags":["tag1","tag2"],"click":"https://example.com","icon":"https://www.guanguans.cn","attachment":{"name":"file.jpg","url":"https://www.guanguans.cn"}}'),
+            create_response('{"id":"ChjDFVOPqaBK","time":1708335367,"expires":1708378567,"event":"message","topic":"guanguans","title":"This is title.","message":"This is message.","priority":1,"tags":["tag1","tag2"],"click":"https://example.com","icon":"https://www.guanguans.cn","attachment":{"name":"file.jpg","url":"https://www.guanguans.cn"}}'),
+        ])
+        ->send($message)->toBeInstanceOf(ResponseInterface::class)
+        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+})->group(__DIR__, __FILE__);
