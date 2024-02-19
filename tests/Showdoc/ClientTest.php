a5dd7ce9dfe22e6bd908e16fd283045f29093b9d
yaozm <ityaozm@gmail.com>
2024-02-19 18:22:38 +0800

    test(Showdoc): Add test for sending message
    - Create ClientTest.php to test sending a message with title and content
    - Use Authenticator, Client, and Message classes
    - Mock responses for success and failure cases
    - Verify the response is an instance of ResponseInterface

---
+++ tests/Showdoc/ClientTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/** @noinspection StaticClosureCanBeUsedInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\NotifyTests\Showdoc;
+
+use Guanguans\Notify\Showdoc\Authenticator;
+use Guanguans\Notify\Showdoc\Client;
+use Guanguans\Notify\Showdoc\Messages\Message;
+use Psr\Http\Message\ResponseInterface;
+
+it('can send message', function (): void {
+    $authenticator = new Authenticator('f096edb95f92540219a41e47060eeb6d946199');
+    $client = new Client($authenticator);
+    $message = Message::make([
+        'title' => 'This is title.',
+        'content' => 'This is content.',
+    ]);
+
+    expect($client)
+        ->httpErrors(false)
+        ->mock([
+            create_response('{"error_code":0,"error_message":"ok"}'),
+            create_response('{"error_code":10103,"error_message":"url\u6216token\u4e0d\u6b63\u786e"}'),
+        ])
+        ->send($message)->toBeInstanceOf(ResponseInterface::class)
+        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+})->group(__DIR__, __FILE__);
