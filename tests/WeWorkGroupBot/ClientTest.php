daf695f8dca15950ac03f6e915a47346c2f13000
yaozm <ityaozm@gmail.com>
2024-02-20 11:01:45 +0800

    feat(ClientTest): add support for sending different types of messages
    - Added support for sending text, image, markdown, and news messages
    - Implemented sending text, image, markdown, and news messages using the WeWorkGroupBot client
    - Added tests for sending text, image, markdown, and news messages
    - Mocked responses for successful and error scenarios

--- tests/WeWorkGroupBot/ClientTest.php
+++ tests/WeWorkGroupBot/ClientTest.php
@@ -17,16 +17,71 @@ namespace Guanguans\NotifyTests\WeWorkGroupBot;
 
 use Guanguans\Notify\WeWorkGroupBot\Authenticator;
 use Guanguans\Notify\WeWorkGroupBot\Client;
+use Guanguans\Notify\WeWorkGroupBot\Messages\ImageMessage;
+use Guanguans\Notify\WeWorkGroupBot\Messages\MarkdownMessage;
+use Guanguans\Notify\WeWorkGroupBot\Messages\NewsMessage;
 use Guanguans\Notify\WeWorkGroupBot\Messages\TextMessage;
 use Psr\Http\Message\ResponseInterface;
 
-it('can send message', function (): void {
+it('can send text message', function (): void {
     $authenticator = new Authenticator('73a3d5a3-ceff-4da8-bcf3-ff5891778');
     $client = new Client($authenticator);
     $message = TextMessage::make([
         'content' => 'This is content.',
         // 'mentioned_list' => ['wangqing', '@all'],
         // 'mentioned_mobile_list' => ['13800001111', '@all'],
+    ]);
+
+    expect($client)
+        ->httpErrors(false)
+        ->mock([
+            create_response('{"errcode":0,"errmsg":"ok"}'),
+            create_response('{"errcode":93000,"errmsg":"invalid webhook url, hint: [1708397705432012366598976], from ip: 218.72.126.124, more info at https://open.work.weixin.qq.com/devtool/query?e=93000"}'),
+        ])
+        ->send($message)->toBeInstanceOf(ResponseInterface::class)
+        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+})->group(__DIR__, __FILE__);
+
+it('can send image message', function (): void {
+    $authenticator = new Authenticator('73a3d5a3-ceff-4da8-bcf3-ff5891778');
+    $client = new Client($authenticator);
+    $message = ImageMessage::make()->image('https://avatars.githubusercontent.com/u/22309277?v=4');
+
+    expect($client)
+        ->httpErrors(false)
+        ->mock([
+            create_response('{"errcode":0,"errmsg":"ok"}'),
+            create_response('{"errcode":93000,"errmsg":"invalid webhook url, hint: [1708397705432012366598976], from ip: 218.72.126.124, more info at https://open.work.weixin.qq.com/devtool/query?e=93000"}'),
+        ])
+        ->send($message)->toBeInstanceOf(ResponseInterface::class)
+        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+})->group(__DIR__, __FILE__);
+
+it('can send markdown message', function (): void {
+    $authenticator = new Authenticator('73a3d5a3-ceff-4da8-bcf3-ff5891778');
+    $client = new Client($authenticator);
+    $message = MarkdownMessage::make()->content("# This is title.\n This is content.");
+
+    expect($client)
+        ->httpErrors(false)
+        ->mock([
+            create_response('{"errcode":0,"errmsg":"ok"}'),
+            create_response('{"errcode":93000,"errmsg":"invalid webhook url, hint: [1708397705432012366598976], from ip: 218.72.126.124, more info at https://open.work.weixin.qq.com/devtool/query?e=93000"}'),
+        ])
+        ->send($message)->toBeInstanceOf(ResponseInterface::class)
+        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+})->group(__DIR__, __FILE__);
+
+it('can send news message', function (): void {
+    $authenticator = new Authenticator('73a3d5a3-ceff-4da8-bcf3-ff5891778');
+    $client = new Client($authenticator);
+    $message = NewsMessage::make()->articles([
+        [
+            'title' => 'This is title1.',
+            'description' => 'This is description.',
+            'url' => 'https://github.com/guanguans/notify',
+            'picurl' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        ],
     ]);
 
     expect($client)
