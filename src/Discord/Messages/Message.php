a111bf5090585202dd0b9de5f2e09fd553358354
yaozm <ityaozm@gmail.com>
2024-02-28 18:31:36 +0800

    refactor(Message): update message class properties and methods
    - Added new properties to the message class: 'allowed_mentions', 'components', 'files', 'payload_json', 'attachments'
    - Updated the allowedTypes array with new entries for 'allowed_mentions', 'components', 'files', 'attachments'
    - Refactored the addEmbed method to include additional fields like 'video', 'provider'
    - Updated the options for 'color', 'footer', 'image', 'thumbnail', 'video', 'provider', 'author' to include new defined fields
    - Updated the README file with additional Discord links

--- src/Discord/Messages/Message.php
+++ src/Discord/Messages/Message.php
@@ -17,11 +17,16 @@ use Guanguans\Notify\Foundation\Concerns\AsPost;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
+ * @method self content($content)
+ * @method self embeds(array $embeds)
+ * @method self allowedMentions(array $allowedMentions)
+ * @method self components(array $components)
+ * @method self files(array $files)
+ * @method self payloadJson($payloadJson)
+ * @method self attachments(array $attachments)
  * @method self username($username)
  * @method self avatarUrl($avatarUrl)
- * @method self content($content)
  * @method self tts($tts)
- * @method self embeds(array $embeds)
  */
 class Message extends \Guanguans\Notify\Foundation\Message
 {
@@ -29,11 +34,17 @@ class Message extends \Guanguans\Notify\Foundation\Message
     use AsPost;
 
     protected array $defined = [
+        'content',
+        'embeds',
+        'allowed_mentions',
+        'components',
+        'files',
+        'payload_json',
+        'attachments',
+
         'username',
         'avatar_url',
-        'content',
         'tts',
-        'embeds',
     ];
 
     protected array $options = [
@@ -43,6 +54,10 @@ class Message extends \Guanguans\Notify\Foundation\Message
     protected array $allowedTypes = [
         'tts' => 'bool',
         'embeds' => 'array',
+        'allowed_mentions' => 'array',
+        'components' => 'array',
+        'files' => 'array',
+        'attachments' => 'array',
     ];
 
     public function addEmbed(array $embed): self
@@ -56,23 +71,72 @@ class Message extends \Guanguans\Notify\Foundation\Message
                         'type',
                         'description',
                         'url',
-                        'color',
                         'timestamp',
+                        'color',
                         'footer',
                         'image',
                         'thumbnail',
+                        'video',
+                        'provider',
                         'author',
                         'fields',
                     ])
-                    ->setAllowedTypes('footer', 'array')
-                    ->setAllowedTypes('image', 'array')
-                    ->setAllowedTypes('thumbnail', 'array')
-                    ->setAllowedTypes('author', 'array')
-                    ->setAllowedTypes('fields', 'array')
                     ->setNormalizer('color', static fn (
                         OptionsResolver $optionsResolver,
                         $value
-                    ) => \is_int($value) ? $value : hexdec($value));
+                    ) => \is_int($value) ? $value : hexdec($value))
+                    ->setAllowedTypes('footer', 'array')
+                    ->setDefault('footer', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'text',
+                            'icon_url',
+                            'proxy_icon_url',
+                        ]);
+                    })
+                    ->setAllowedTypes('image', 'array')
+                    ->setDefault('image', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'url',
+                            'proxy_url',
+                            'height',
+                            'width',
+                        ]);
+                    })
+                    ->setAllowedTypes('thumbnail', 'array')
+                    ->setDefault('thumbnail', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'url',
+                            'proxy_url',
+                            'height',
+                            'width',
+                        ]);
+                    })
+                    ->setAllowedTypes('video', 'array')
+                    ->setDefault('video', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'url',
+                            'proxy_url',
+                            'height',
+                            'width',
+                        ]);
+                    })
+                    ->setAllowedTypes('provider', 'array')
+                    ->setDefault('provider', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'name',
+                            'url',
+                        ]);
+                    })
+                    ->setAllowedTypes('author', 'array')
+                    ->setDefault('author', static function (OptionsResolver $optionsResolver): void {
+                        $optionsResolver->setDefined([
+                            'name',
+                            'url',
+                            'icon_url',
+                            'proxy_icon_url',
+                        ]);
+                    })
+                    ->setAllowedTypes('fields', 'array');
             }
         );
 
--- src/Discord/README.md
+++ src/Discord/README.md
@@ -5,3 +5,5 @@
 ## Related links
 
 * [https://discord.com/developers/docs/resources/webhook#edit-webhook-message](https://discord.com/developers/docs/resources/webhook#edit-webhook-message)
+* [https://discord.com/channels/@me](https://discord.com/channels/@me)
+* [https://support.discord.com/hc/zh-tw/articles/228383668-%E4%BD%BF%E7%94%A8%E7%B6%B2%E7%B5%A1%E9%89%A4%E6%89%8B-Webhooks](https://support.discord.com/hc/zh-tw/articles/228383668-%E4%BD%BF%E7%94%A8%E7%B6%B2%E7%B5%A1%E9%89%A4%E6%89%8B-Webhooks)
