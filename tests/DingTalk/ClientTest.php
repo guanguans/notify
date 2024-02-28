c1a56398b0c0b192a6f22521a4afd43b92d5fac3
yaozm <ityaozm@gmail.com>
2024-02-28 17:07:27 +0800

    refactor(tests): Update ClientTest.php for message sending functionality
    - Refactored the `ClientTest.php` file to improve the message sending functionality
    - Added test cases for sending various types of messages
    - Updated the test cases to use the `assertCanSendMessage` method
    - Removed unnecessary code related to authentication
    - Organized the test cases into different groups

--- tests/DingTalk/ClientTest.php
+++ tests/DingTalk/ClientTest.php
@@ -23,145 +23,104 @@ use Guanguans\Notify\DingTalk\Messages\LinkMessage;
 use Guanguans\Notify\DingTalk\Messages\MarkdownMessage;
 use Guanguans\Notify\DingTalk\Messages\SingleActionCardMessage;
 use Guanguans\Notify\DingTalk\Messages\TextMessage;
-use Psr\Http\Message\ResponseInterface;
 
-it('can send text message', function (): void {
+beforeEach(function (): void {
     $authenticator = new Authenticator(
         'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
         'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
     );
-    $client = new Client($authenticator);
+    $this->client = (new Client($authenticator))->mock([
+        create_response('{"errcode":0,"errmsg":"ok"}'),
+        create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
+    ]);
+});
+
+it('can send text message', function (): void {
     $message = TextMessage::make([
         'content' => 'This is content(keyword).',
-        // 'atMobiles' => [13948484984],
-        // 'atDingtalkIds' => [123456],
-        // 'isAtAll'   => false,
+        'atMobiles' => [13948484984],
+        'atDingtalkIds' => [123456],
+        'isAtAll' => false,
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
 
 it('can send btns action card message', function (): void {
-    $authenticator = new Authenticator(
-        'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
-        'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
-    );
-    $client = new Client($authenticator);
     $message = BtnsActionCardMessage::make([
         'title' => 'This is title(keyword).',
         'text' => 'This is text.',
-        // 'hideAvatar'     => 1,
-        // 'btnOrientation' => 1,
+        'btnOrientation' => 1,
         'btns' => [
             [
-                'title' => 'This is title 1',
+                'title' => 'This is title 1.',
+                'actionURL' => 'https://github.com/guanguans/notify',
+            ],
+            [
+                'title' => 'This is title 2.',
                 'actionURL' => 'https://github.com/guanguans/notify',
             ],
         ],
+    ])->addBtn([
+        'title' => 'This is title 3.',
+        'actionURL' => 'https://github.com/guanguans/notify',
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
 
 it('can send feed card message', function (): void {
-    $authenticator = new Authenticator(
-        'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
-        'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
-    );
-    $client = new Client($authenticator);
     $message = FeedCardMessage::make()->links([
         [
-            'title' => 'This is title(keyword) 0.',
+            'title' => 'This is title(keyword) 1.',
             'messageURL' => 'https://github.com/guanguans/notify',
             'picURL' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
         ],
+        [
+            'title' => 'This is title(keyword) 2.',
+            'messageURL' => 'https://github.com/guanguans/notify',
+            'picURL' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        ],
+    ])->addLink([
+        'title' => 'This is title(keyword) 3.',
+        'messageURL' => 'https://github.com/guanguans/notify',
+        'picURL' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
 
 it('can send link message', function (): void {
-    $authenticator = new Authenticator(
-        'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
-        'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
-    );
-    $client = new Client($authenticator);
     $message = LinkMessage::make([
-        'title' => 'This is content.',
+        'title' => 'This is title.',
         'text' => 'This is text(keyword).',
         'messageUrl' => 'https://github.com/guanguans/notify',
         'picUrl' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
 
 it('can send markdown message', function (): void {
-    $authenticator = new Authenticator(
-        'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
-        'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
-    );
-    $client = new Client($authenticator);
     $message = MarkdownMessage::make([
         'title' => 'This is title.',
         'text' => '> This is text(keyword).',
-        // 'atMobiles' => [13948484984],
-        // 'atDingtalkIds' => [123456],
-        // 'isAtAll'   => false,
+        'atMobiles' => [13948484984],
+        'atDingtalkIds' => [123456],
+        'isAtAll' => false,
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
 
 it('can send single action card message', function (): void {
-    $authenticator = new Authenticator(
-        'c44fec1ddaa8a833156efb77b7865d62ae13775418030d94d05da08bfca73',
-        'SECc32bb7345c0f73da2b9786f0f7dd5083bd768a29b82e6d460149d730eee51730'
-    );
-    $client = new Client($authenticator);
     $message = SingleActionCardMessage::make([
         'title' => 'This is title(keyword).',
         'text' => 'This is text.',
         'singleTitle' => 'This is singleTitle.',
         'singleURL' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        // 'btnOrientation' => 1
+        'btnOrientation' => 1,
     ]);
 
-    expect($client)
-        ->mock([
-            create_response('{"errcode":0,"errmsg":"ok"}'),
-            create_response('{"errcode":300005,"errmsg":"token is not exist"}'),
-        ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+    expect($this->client)->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
