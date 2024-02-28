72e5119ea6e9554a3cdd0400cbaf5ce9f1efe294
yaozm <ityaozm@gmail.com>
2024-02-28 18:36:49 +0800

    feat(Discord): Add ability to send messages with rich content
    - Updated message creation to include rich content features such as username, avatar, tts, and embeds
    - Added details for embeds including title, description, color, timestamp, footer, image, thumbnail, video, provider, author, and fields

--- tests/Discord/ClientTest.php
+++ tests/Discord/ClientTest.php
@@ -18,52 +18,71 @@ namespace Guanguans\NotifyTests\Discord;
 use Guanguans\Notify\Discord\Authenticator;
 use Guanguans\Notify\Discord\Client;
 use Guanguans\Notify\Discord\Messages\Message;
-use Psr\Http\Message\ResponseInterface;
 
 it('can send message', function (): void {
     $authenticator = new Authenticator('https://discord.com/api/webhooks/1201788029790855200/rsFsK_vmdFLZBqWU3w3En4aVWsekGllzd-mOCby8ymO459G53EBu9FFcIuwCnaXYx');
     $client = new Client($authenticator);
     $message = Message::make([
         'content' => 'This is content.',
-        // 'username' => 'notify bot.',
-        // 'avatar_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        // 'tts' => false,
-        // 'embeds' => $embed = [
-        //     'title' => 'This is title.',
-        //     'type' => 'This is type.',
-        //     'description' => 'This is description.',
-        //     'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        //     'color' => '0365D6',
-        //     'footer' => [
-        //         'text' => 'This is text.',
-        //         'icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        //     ],
-        //     'image' => [
-        //         'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        //     ],
-        //     'thumbnail' => [
-        //         'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        //     ],
-        //     'author' => [
-        //         'name' => 'This is name.',
-        //         'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4.',
-        //         'icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        //     ],
-        //     'fields' => [
-        //         [
-        //             'name' => 'This is name.',
-        //             'value' => 'This is value.',
-        //             'inline' => false,
-        //         ],
-        //     ],
-        // ],
-    ]);
+        'username' => 'This is username.',
+        'avatar_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        'tts' => true,
+        'embeds' => [
+            $embed = [
+                'title' => 'This is title.',
+                'type' => 'rich',
+                'description' => 'This is description.',
+                'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                'timestamp' => '2022-02-24T09:30:00Z',
+                'color' => hexdec('0365D6'),
+                'footer' => [
+                    'text' => 'This is text.',
+                    'icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'proxy_icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                ],
+                'image' => [
+                    'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'proxy_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'height' => 50,
+                    'width' => 50,
+                ],
+                'thumbnail' => [
+                    'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'proxy_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'height' => 50,
+                    'width' => 50,
+                ],
+                'video' => [
+                    'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'proxy_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'height' => 50,
+                    'width' => 50,
+                ],
+                'provider' => [
+                    'name' => 'This is name.',
+                    'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4.',
+                ],
+                'author' => [
+                    'name' => 'This is name.',
+                    'url' => 'https://avatars.githubusercontent.com/u/22309277?v=4.',
+                    'icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                    'proxy_icon_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+                ],
+                'fields' => [
+                    [
+                        'name' => 'This is name.',
+                        'value' => 'This is value.',
+                        'inline' => false,
+                    ],
+                ],
+            ],
+        ],
+    ])->addEmbed($embed);
 
     expect($client)
         ->mock([
             create_response('', 204),
             create_response('{"message": "Invalid Webhook Token", "code": 50027}', 401),
         ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+        ->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
