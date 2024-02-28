f42b584039f6b0e3bf40af4922719a556019bac2
yaozm <ityaozm@gmail.com>
2024-02-28 10:46:40 +0800

    test(ClientTest): Refactor message creation in ClientTest
    - Removed unused fields
    - Commented out unused fields
    - Refactored message creation in ClientTest

--- tests/Bark/ClientTest.php
+++ tests/Bark/ClientTest.php
@@ -26,16 +26,15 @@ it('can send message', function (): void {
     $message = Message::make([
         'title' => 'This is title.',
         'body' => 'This is body.',
-        'copy' => 'This is copy.',
-        'url' => 'https://github.com/guanguans/notify',
-        'sound' => 'bell',
-        'group' => 'group',
-        // 'icon' => 'https://avatars0.githubusercontent.com/u/25671453?s=200&v=4',
-        // 'level' => 'passive',
-        // 'badge' => 5,
-        // 'isArchive' => 1,
+        // 'level' => 'active',
+        // 'badge' => 3,
         // 'autoCopy' => 1,
-        // 'automaticallyCopy' => 1,
+        // 'copy' => 'This is copy.',
+        // 'sound' => 'bell',
+        // 'icon' => 'https://avatars0.githubusercontent.com/u/25671453?s=200&v=4',
+        // 'group' => 'This is group.',
+        // 'isArchive' => 1,
+        // 'url' => 'https://github.com/guanguans/notify',
     ]);
 
     expect($client)
