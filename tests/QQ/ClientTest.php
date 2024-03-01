797976c95bcfd42837667c1b0edef7207d4ab4c1
yaozm <ityaozm@gmail.com>
2024-03-01 21:30:33 +0800

    test(ClientTest): refactor message creation in ClientTest
    - Refactored the creation of the message in the ClientTest
    - Added 'image' and 'file_image' fields with appropriate values
    - Updated 'msg_id' field with a new value
    - Removed unnecessary fields like 'event_id' and 'markdown'

--- tests/QQ/ClientTest.php
+++ tests/QQ/ClientTest.php
@@ -18,8 +18,6 @@ namespace Guanguans\NotifyTests\QQ;
 use Guanguans\Notify\QQ\Authenticator;
 use Guanguans\Notify\QQ\Client;
 use Guanguans\Notify\QQ\Messages\Message;
-use Guanguans\Notify\QQ\Messages\SandboxMessage;
-use Psr\Http\Message\ResponseInterface;
 
 it('can send message', function (): void {
     $authenticator = new Authenticator(
@@ -27,29 +25,23 @@ it('can send message', function (): void {
         'eghXYBXQH0QXBByb8Zj4VeRGterQG',
     );
     $client = new Client($authenticator);
-    $message = Message::make($data = [
+    $message = Message::make([
         'channel_id' => '4316959',
-
         'content' => 'This is content.',
-        'msg_id' => '3yfBSa',
-        // 'file_image' => '/Users/yaozm/Downloads/images.jpeg',
-        // 'image' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
-        // 'event_id' => 'event_id',
-        // 'embed' => [],
-        // 'ark' => [],
-        // 'message_reference' => [],
-        // 'markdown' => [],
+        'embed' => [],
+        'ark' => [],
+        'message_reference' => [],
+        'image' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        'file_image' => 'tests/fixtures/image.png',
+        'msg_id' => 'This is msg id.',
+        // 'event_id' => 'This is event id.',
+        'markdown' => [],
     ]);
-    $sandboxMessage = SandboxMessage::make($data);
 
     expect($client)
         ->mock([
             create_response('{"id":"08efdfa1b3e1d4d6e246109fbe8702383048e4d0ccae06","channel_id":"4316959","guild_id":"5099581822453968879","content":"This is content.","timestamp":"2024-02-19T18:07:32+08:00","tts":false,"mention_everyone":false,"author":{"id":"7938900097687957410","username":"","avatar":"","bot":true},"pinned":false,"type":0,"flags":0,"seq_in_channel":"48"}'),
-            create_response(
-                '{"message":"Token错误","code":11243,"err_code":40012002,"trace_id":"b363de10b4c8893a382b8b9d6d07c615"}',
-                401
-            ),
+            create_response('{"message":"Token错误","code":11243,"err_code":40012002,"trace_id":"b363de10b4c8893a382b8b9d6d07c615"}', 401),
         ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($sandboxMessage)->toBeInstanceOf(ResponseInterface::class);
-})->group(__DIR__, __FILE__)->skip();
+        ->assertCanSendMessage($message);
+})->group(__DIR__, __FILE__);
