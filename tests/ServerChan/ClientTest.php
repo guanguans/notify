c6afe039e6778befa62caeb394c927c45ff7a83b
yaozm <ityaozm@gmail.com>
2024-03-01 23:27:12 +0800

    test(ClientTest): Add new message fields
    - Added 'short', 'noip', and 'channel' fields to the message
    - Commented out 'openid' field
    - Updated test assertions to accommodate new message fields

--- tests/ServerChan/ClientTest.php
+++ tests/ServerChan/ClientTest.php
@@ -18,7 +18,6 @@ namespace Guanguans\NotifyTests\ServerChan;
 use Guanguans\Notify\ServerChan\Authenticator;
 use Guanguans\Notify\ServerChan\Client;
 use Guanguans\Notify\ServerChan\Messages\Message;
-use Psr\Http\Message\ResponseInterface;
 
 it('can send message', function (): void {
     $authenticator = new Authenticator('SCT35149Thtf1g2Bc14QJuQ6HFpW5Y');
@@ -26,6 +25,10 @@ it('can send message', function (): void {
     $message = Message::make([
         'title' => 'This is title.',
         'desp' => 'This is desp.',
+        'short' => 'This is short.',
+        'noip' => 0,
+        'channel' => '1|9',
+        // 'openid' => 'f096edb95f92540219a41e4706',
     ]);
 
     expect($client)
@@ -33,6 +36,5 @@ it('can send message', function (): void {
             create_response('{"code":0,"message":"","data":{"pushid":"156157448","readkey":"SCTRd7BkgAHn6XG","error":"SUCCESS","errno":0}}'),
             create_response('{"message":"[AUTH]\u9519\u8bef\u7684Key","code":40001,"info":"\u9519\u8bef\u7684Key","args":[null],"scode":461}', 400),
         ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+        ->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
