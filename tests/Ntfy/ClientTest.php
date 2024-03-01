c465b0884bb28d557cc54198e0b637ad14f9d573
yaozm <ityaozm@gmail.com>
2024-03-01 15:40:50 +0800

    test(ClientTest): update message sending feature
    - Updated message sending feature in ClientTest
    - Added new message attributes like actions, click, attach, markdown, icon, filename, and email
    - Removed unnecessary attributes like priority, cache, firebase
    - Added action to open portal with specific label and URL

--- tests/Ntfy/ClientTest.php
+++ tests/Ntfy/ClientTest.php
@@ -17,7 +17,6 @@ namespace Guanguans\NotifyTests\Ntfy;
 
 use Guanguans\Notify\Ntfy\Client;
 use Guanguans\Notify\Ntfy\Messages\Message;
-use Psr\Http\Message\ResponseInterface;
 
 it('can send message', function (): void {
     $client = new Client;
@@ -25,24 +24,31 @@ it('can send message', function (): void {
         'topic' => 'guanguans',
         'message' => 'This is message.',
         'title' => 'This is title.',
-        'priority' => 1,
         'tags' => ['tag1', 'tag2'],
-        'click' => 'https://example.com',
-        'attach' => 'https://www.guanguans.cn',
-        'icon' => 'https://www.guanguans.cn',
-        'filename' => 'file.jpg',
+        'priority' => 1,
+        'actions' => [],
+        'click' => 'https://www.guanguans.cn',
+        'attach' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        'markdown' => true,
+        'icon' => 'https://avatars0.githubusercontent.com/u/25671453?s=200&v=4',
+        'filename' => 'avatar.png',
+        // 'delay' => '30min, 9am',
+        'email' => 'xxx@qq.com',
+        // 'call' => '+8613948484984',
         'cache' => 'no',
         'firebase' => 'no',
-        // 'actions' => [],
-        // 'delay' => '30min, 9am',
-        // 'email' => 'xxx@qq.com',
+    ])->addAction([
+        'action' => 'view',
+        'label' => 'Open portal',
+        'url' => 'https://home.nest.com/',
+        'clear' => true,
     ]);
 
     expect($client)
         ->mock([
             create_response('{"id":"ChjDFVOPqaBK","time":1708335367,"expires":1708378567,"event":"message","topic":"guanguans","title":"This is title.","message":"This is message.","priority":1,"tags":["tag1","tag2"],"click":"https://example.com","icon":"https://www.guanguans.cn","attachment":{"name":"file.jpg","url":"https://www.guanguans.cn"}}'),
-            create_response('{"id":"ChjDFVOPqaBK","time":1708335367,"expires":1708378567,"event":"message","topic":"guanguans","title":"This is title.","message":"This is message.","priority":1,"tags":["tag1","tag2"],"click":"https://example.com","icon":"https://www.guanguans.cn","attachment":{"name":"file.jpg","url":"https://www.guanguans.cn"}}'),
+            create_response('{"code":40035,"http":400,"error":"invalid request: anonymous phone calls are not allowed","link":"https://ntfy.sh/docs/publish/#phone-calls"}', 400),
+            create_response('{"code":40003,"http":400,"error":"delayed e-mail notifications are not supported"}', 400),
         ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+        ->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
