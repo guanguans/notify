f95b2697e25204b45345b53fc66c483b62d69bbd
yaozm <ityaozm@gmail.com>
2024-03-01 23:01:20 +0800

    feat(RocketChat): Add message sending functionality with attachments
    - Added message sending functionality with attachments
    - Included text, alias, emoji, avatar, tshow, and attachments
    - Added two attachments with titles, links, text, and image URLs

--- tests/RocketChat/ClientTest.php
+++ tests/RocketChat/ClientTest.php
@@ -18,24 +18,33 @@ namespace Guanguans\NotifyTests\RocketChat;
 use Guanguans\Notify\RocketChat\Authenticator;
 use Guanguans\Notify\RocketChat\Client;
 use Guanguans\Notify\RocketChat\Messages\Message;
-use Psr\Http\Message\ResponseInterface;
 
 it('can send message', function (): void {
     $authenticator = new Authenticator('https://guanguans.rocket.chat/hooks/65bf67eb387fb98fc8bcc245/fcN2v4EkkkaLM8wzgfSeEWD9ngjgLPBhspPPFhoM78Ww2');
     $client = new Client($authenticator);
     $message = Message::make([
-        'alias' => '报警机器人',
+        // 'roomId' => 'Xnb2kLD2Pnhdwe3RH',
+        // 'channel' => '#general',
+        'text' => 'This is text. ',
+        'alias' => 'This is alias.',
         'emoji' => ':warning:',
-        'text' => 'This is a text. ',
-        // 'attachments' => [
-        //     [
-        //         'title' => 'This is a title.',
-        //         'title_link' => 'https://rocket.chat',
-        //         'text' => 'This is a text.',
-        //         'image_url' => 'http://www.xxx.png',
-        //         'color' => '#764FA5',
-        //     ],
-        // ],
+        'avatar' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        // 'tmid' => 'jC9chsFddTvsbFQG7',
+        'tshow' => true,
+        'attachments' => [
+            [
+                'title' => 'This is title 1.',
+                'title_link' => 'https://rocket.chat',
+                'text' => 'This is text 1.',
+                'color' => '#BB3E4E',
+            ],
+        ],
+    ])->addAttachment([
+        'title' => 'This is title 2.',
+        'title_link' => 'https://rocket.chat',
+        'text' => 'This is text 2.',
+        'image_url' => 'https://avatars.githubusercontent.com/u/22309277?v=4',
+        'color' => '#764FA5',
     ]);
 
     expect($client)
@@ -43,6 +52,5 @@ it('can send message', function (): void {
             create_response('{"success":true}'),
             create_response('{"success":false,"error":"Invalid integration id or token provided."}'),
         ])
-        ->send($message)->toBeInstanceOf(ResponseInterface::class)
-        ->send($message)->toBeInstanceOf(ResponseInterface::class);
+        ->assertCanSendMessage($message);
 })->group(__DIR__, __FILE__);
