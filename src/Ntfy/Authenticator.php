313ba070a5db1736ef703027fb8395f1fb650a30
yaozm <ityaozm@gmail.com>
2024-03-08 15:45:56 +0800

    refactor(Authenticator): improve constructor logic
    - Added new authenticators: NullAuthenticator, OptionsAuthenticator, BasicAuthenticator
    - Updated constructor to handle different argument scenarios
    - Improved error handling for invalid number of arguments

--- src/Ntfy/Authenticator.php
+++ src/Ntfy/Authenticator.php
@@ -12,6 +12,44 @@ declare(strict_types=1);
 
 namespace Guanguans\Notify\Ntfy;
 
+use Guanguans\Notify\Foundation\Authenticators\AggregateAuthenticator;
+use Guanguans\Notify\Foundation\Authenticators\BasicAuthenticator;
 use Guanguans\Notify\Foundation\Authenticators\BearerAuthenticator;
+use Guanguans\Notify\Foundation\Authenticators\NullAuthenticator;
+use Guanguans\Notify\Foundation\Authenticators\OptionsAuthenticator;
+use Guanguans\Notify\Foundation\Exceptions\InvalidArgumentException;
+use GuzzleHttp\RequestOptions;
 
-class Authenticator extends BearerAuthenticator {}
+class Authenticator extends AggregateAuthenticator
+{
+    public function __construct(?string $usernameOrToken = null, ?string $password = null)
+    {
+        switch (\func_num_args()) {
+            case 0:
+                $authenticators = [
+                    new NullAuthenticator,
+                ];
+
+                break;
+            case 1:
+                $authenticators = [
+                    new BearerAuthenticator($usernameOrToken),
+                    new OptionsAuthenticator([
+                        RequestOptions::QUERY => [RequestOptions::AUTH => $usernameOrToken],
+                    ]),
+                ];
+
+                break;
+            case 2:
+                $authenticators = [
+                    new BasicAuthenticator($usernameOrToken, $password),
+                ];
+
+                break;
+            default:
+                throw new InvalidArgumentException('The number of arguments must be 0, 1 or 2.');
+        }
+
+        parent::__construct(...$authenticators);
+    }
+}
