bf9a95c036af796ae1495799d9cb82870d0ebbd4
yaozm <ityaozm@gmail.com>
2024-06-14 14:18:49 +0800

    test(test): add tests for form params, query, and Multipart conversion
    - Added tests for converting message to form params, query, and Multipart
    - Ensured conversion methods return the expected types

--- tests/Foundation/MessageTest.php
+++ tests/Foundation/MessageTest.php
@@ -18,9 +18,28 @@ namespace Guanguans\NotifyTests\Foundation;
 
 use Guanguans\Notify\Foundation\Concerns\AsNullUri;
 use Guanguans\Notify\Foundation\Message;
+use GuzzleHttp\Psr7\MultipartStream;
 
 it('can dump debug info', function (): void {
     expect(new class extends Message {
         use AsNullUri;
     })->dump()->toBeInstanceOf(Message::class);
+})->group(__DIR__, __FILE__);
+
+it('can to form params', function (): void {
+    expect((fn (): string => $this->toFormParams())->call(new class extends Message {
+        use AsNullUri;
+    }))->toBeString();
+})->group(__DIR__, __FILE__);
+
+it('can to query', function (): void {
+    expect((fn (): string => $this->toQuery())->call(new class extends Message {
+        use AsNullUri;
+    }))->toBeString();
+})->group(__DIR__, __FILE__);
+
+it('can to Multipart', function (): void {
+    expect((fn (): MultipartStream => $this->toMultiPart())->call(new class extends Message {
+        use AsNullUri;
+    }))->toBeInstanceOf(MultipartStream::class);
 })->group(__DIR__, __FILE__);
