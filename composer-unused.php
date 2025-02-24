5f9832a11e903a41a7cae16dab5f855f8e4fb5a7
guanguans <ityaozm@gmail.com>
2025-02-25 00:03:37 +0800

    ci(config): Update dependencies and improve PHPStan configuration
    - Update `illuminate/collections` and `illuminate/support` dependencies to include version `^12.0`.
    - Add new dependencies: `phpstan/phpstan-strict-rules` and `spaze/phpstan-disallowed-calls`.
    - Modify PHPStan configuration to include disallowed function calls and exclude specific paths.
    - Simplify additional files retrieval in `composer-unused.php`.

--- composer-unused.php
+++ composer-unused.php
@@ -14,15 +14,8 @@ declare(strict_types=1);
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
-use Webmozart\Glob\Glob;
 
 return static fn (Configuration $configuration): Configuration => $configuration
     // ->addNamedFilter(NamedFilter::fromString('symfony/config'))
     // ->addPatternFilter(PatternFilter::fromString('/symfony\/.*/'))
-    ->setAdditionalFilesFor(
-        'icanhazstring/composer-unused',
-        array_merge(
-            Glob::glob(__DIR__.'/*.php'),
-            Glob::glob(__DIR__.'/.*.php'),
-        ),
-    );
+    ->setAdditionalFilesFor('icanhazstring/composer-unused', glob(__DIR__.'/{*,.*}.php', \GLOB_BRACE));
