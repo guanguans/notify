0562a05be0415a9248b408dae62458ac944db15b
guanguans <ityaozm@gmail.com>
2025-03-08 20:06:02 +0800

    ci(dependency): Add composer dependency analyser configuration
    - Introduce `composer-dependency-analyser.php` to manage dependencies
    - Remove outdated `composer-require-checker.json` and `composer-unused.php` files
    - Update `composer.json` to reflect dependency changes and include new scripts
    - Enhance error handling and path exclusions for better analysis

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/notify
+ */
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration)
+    ->addPathsToScan(
+        [
+            __DIR__.'/src',
+        ],
+        false
+    )
+    ->addPathsToExclude([
+        __DIR__.'/tests',
+        __DIR__.'/src/Foundation/Rectors',
+    ])
+    ->ignoreUnknownClasses([
+        // SensitiveParameter::class,
+        'SensitiveParameter',
+    ])
+    ->ignoreErrorsOnExtensions(
+        [
+            'ext-ctype',
+            'ext-curl',
+            'ext-filter',
+            'ext-mbstring',
+        ],
+        [ErrorType::SHADOW_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackages(
+        [
+            'guzzlehttp/promises',
+            'guzzlehttp/psr7',
+            'psr/http-factory',
+            'psr/http-message',
+        ],
+        [ErrorType::SHADOW_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackages(
+        [
+            'guanguans/ai-commit',
+            'illuminate/collections',
+            'illuminate/support',
+            'symfony/var-dumper',
+        ],
+        [ErrorType::DEV_DEPENDENCY_IN_PROD]
+    );
